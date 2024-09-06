01ba91636d5e3b713c70a9feda559fa21eccef4f
VincentB00 <vincentbui1999@gmail.com>
2024-09-06 14:14:55 -0400

    add debug wrap

--- projects/wrap/src/app/app.module.ts
+++ projects/wrap/src/app/app.module.ts
@@ -3,13 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ViescloudUtilsModule } from 'projects/viescloud-utils/src/public-api';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule,
+    ViescloudUtilsModule,
     AppRoutingModule
   ],
   providers: [],
