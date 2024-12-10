cc6bf460ad7974577f0df6e9108cdbf8107c73a5
VincentB00 <vincentbui1999@gmail.com>
2024-12-10 22:06:35 +0000

    update

--- projects/ensible/src/app/item/ensible-item/ensible-item.component.ts
+++ projects/ensible/src/app/item/ensible-item/ensible-item.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { EnsibleItemService } from '../../service/ensible-item/ensible-item.service';
 import { EnsibleItem } from '../../model/ensible.model';
 import { RouteUtils } from 'projects/viescloud-utils/src/lib/util/Route.utils';
+import { RxJSUtils } from 'projects/viescloud-utils/src/lib/util/RxJS.utils';
+import { DataUtils } from 'projects/viescloud-utils/src/lib/util/Data.utils';
 
 @Component({
   selector: 'app-ensible-item',
@@ -11,13 +13,40 @@ import { RouteUtils } from 'projects/viescloud-utils/src/lib/util/Route.utils';
 export class EnsibleItemComponent implements OnInit {
   
   item!: EnsibleItem;
+  itemCopy!: EnsibleItem;
   blankItem: EnsibleItem = new EnsibleItem();
+
+  validForm: boolean = false;
   
   constructor(
-    private ensibleItemService: EnsibleItemService
+    private ensibleItemService: EnsibleItemService,
+    private rxjsUtils: RxJSUtils
   ) { }
 
   ngOnInit(): void {
+    let id = RouteUtils.getPathVariable('item');
+    if(id === '0') {
+      this.item = new EnsibleItem();
+    }
+    else {
+      this.ensibleItemService.get(id).pipe(this.rxjsUtils.waitLoadingDialog()).subscribe({
+        next: res => {
+          this.item = res;
+          this.itemCopy = structuredClone(this.item);
+        }
+      })
+    }
+  }
 
+  isValueChange() {
+    return DataUtils.isNotEqual(this.item, this.itemCopy);
+  }
+
+  save() {
+    
+  }
+
+  revert() {
+    this.item = structuredClone(this.itemCopy);
   }
 }
