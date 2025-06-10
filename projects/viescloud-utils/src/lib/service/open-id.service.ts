090a7574a31ee36b6c4498446dedc58c3e9f3b77
VincentB00 <vincentbui1999@gmail.com>
2025-06-10 20:52:09 +0000

    refactor(viescloud-utils): Remove unused dialogs and services
    Removes dialog components, affiliate marketing services, AI services,
    DNS manager services, organization services, raphael services,
    smb services, v-game services, venkins services, and wrap services.
    
    This commit cleans up the codebase by removing unused components and
    services, reducing the overall bundle size and improving maintainability.

--- projects/viescloud-utils/src/lib/service/open-id.service.ts
+++ projects/viescloud-utils/src/lib/service/open-id.service.ts
@@ -1,7 +1,5 @@
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
-import { environment } from 'projects/environments/environment.prod';
-import { StringUtils } from '../util/String.utils';
 
 @Injectable({
   providedIn: 'root'
@@ -17,20 +15,20 @@ export class OpenIdService {
   }
 
   logoutFlow(): void {
-    let logoutUri = environment.authentik_openid_logout_url;
-    this.router.navigate(["/"]).then(result=>{window.location.href = logoutUri;});
+    // let logoutUri = environment.authentik_openid_logout_url;
+    // this.router.navigate(["/"]).then(result=>{window.location.href = logoutUri;});
   }
 
   authorizeFlow(): void {
-    let authentikUrl = environment.authentik_openid_url;
-    let clientId = environment.authentik_client_id;
-    let responseType = 'code';
-    let scope = 'openid+email+profile';
-    let redirectUrl = encodeURIComponent(this.getRedirectUri());
-    let state = StringUtils.makeId(20);
+    // let authentikUrl = environment.authentik_openid_url;
+    // let clientId = environment.authentik_client_id;
+    // let responseType = 'code';
+    // let scope = 'openid+email+profile';
+    // let redirectUrl = encodeURIComponent(this.getRedirectUri());
+    // let state = StringUtils.makeId(20);
 
-    let authenticationUrl = `${authentikUrl}?client_id=${clientId}&response_type=${responseType}&scope=${scope}&redirect_uri=${redirectUrl}&state=${state}`;
+    // let authenticationUrl = `${authentikUrl}?client_id=${clientId}&response_type=${responseType}&scope=${scope}&redirect_uri=${redirectUrl}&state=${state}`;
 
-    this.router.navigate(["/"]).then(result=>{window.location.href = authenticationUrl;});
+    // this.router.navigate(["/"]).then(result=>{window.location.href = authenticationUrl;});
   }
 }
