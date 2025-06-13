80168577d70400dc722a7a49fec5964bcc618d98
VincentB00 <vincentbui1999@gmail.com>
2025-06-13 14:46:54 -0400

    refactor(auth): Improve AuthInterceptor token handling

--- projects/viescloud-utils/src/lib/guards/auth.interceptor.ts
+++ projects/viescloud-utils/src/lib/guards/auth.interceptor.ts
@@ -4,6 +4,7 @@ import { Observable } from 'rxjs';
 import { AuthenticatorService } from '../service/authenticator.service';
 import { environment } from 'projects/environments/environment.prod';
 import { StringUtils } from '../util/String.utils';
+import { ViesService } from '../service/rest.service';
 
 @Injectable({
   providedIn: 'root'
@@ -16,22 +17,35 @@ export class AuthInterceptor implements HttpInterceptor {
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     const authenticatorService = this.injector.get(AuthenticatorService);
 
-    if (!req.url.includes(environment.gateway_api))
+    if (!req.url.includes(ViesService.getUri())) {
       return next.handle(req);
+    }
 
     let body = req.body;
-    let token = authenticatorService.currentJwtToken;
     
-    if (body && typeof body === 'string' && StringUtils.isValidJson(body)) {
-      return next.handle(req.clone({
-        headers: req.headers.set('Content-Type', 'application/json')
-                            .set('Authorization', `Bearer ${token}`),
-      }));
+    if (authenticatorService.isAuthenticatedSync()) {
+      let token = authenticatorService.currentJwtToken;
+      if (body && typeof body === 'string' && StringUtils.isValidJson(body)) {
+        return next.handle(req.clone({
+          headers: req.headers.set('Content-Type', 'application/json')
+                              .set('Authorization', `Bearer ${token}`),
+        }));
+      }
+      else {
+        return next.handle(req.clone({
+          headers: req.headers.set('Authorization', `Bearer ${token}`),
+        }));
+      }
     }
     else {
-      return next.handle(req.clone({
-        headers: req.headers.set('Authorization', `Bearer ${token}`),
-      }));
+      if (body && typeof body === 'string' && StringUtils.isValidJson(body)) {
+        return next.handle(req.clone({
+          headers: req.headers.set('Content-Type', 'application/json')
+        }));
+      }
+      else {
+        return next.handle(req);
+      }
     }
   }
 }
