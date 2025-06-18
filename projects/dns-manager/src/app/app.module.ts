7a862347fcca30c410917921f7d3aa3df13b71a1
VincentB00 <vincentbui1999@gmail.com>
2025-06-17 23:09:53 -0400

    refactor(dns-manager): Remove custom object storage service

--- projects/dns-manager/src/app/app.module.ts
+++ projects/dns-manager/src/app/app.module.ts
@@ -8,8 +8,6 @@ import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { DnsManagerSettingService } from './service/dns-manager.setting.service';
 import { AuthInterceptor } from 'projects/viescloud-utils/src/lib/guards/auth.interceptor';
 import { SettingService } from 'projects/viescloud-utils/src/lib/service/setting.service';
-import { ObjectStorageService } from 'projects/viescloud-utils/src/lib/service/object-storage-manager.service';
-import { DnsManagerObjectStorageService } from './service/dns-manager.object.storage.service';
 import { DnsSettingListComponent } from './dns-setting-list/dns-setting-list.component';
 import { DnsRecordListComponent } from './dns-record-list/dns-record-list.component';
 import { DnsRecordComponent } from './dns-record/dns-record.component';
@@ -38,10 +36,6 @@ const LIST = [
     {
       provide: SettingService,
       useClass: DnsManagerSettingService
-    },
-    {
-      provide: ObjectStorageService,
-      useClass: DnsManagerObjectStorageService
     }
   ],
   bootstrap: [AppComponent]
