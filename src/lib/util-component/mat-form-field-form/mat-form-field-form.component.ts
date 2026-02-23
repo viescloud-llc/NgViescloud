bcd1b5bb1bbed16500fada18ce20b2a78a79aac8
VincentB00 <vincentbui1999@gmail.com>
2026-02-23 17:03:32 -0500

    feat(form): Enhance dynamic forms with dialog support and action visibility
    MatFormFieldFormComponent now detects MatDialog context and clones initial value.
    MatFormFieldInputDynamicFormComponent gains inputs to control revert/remove button visibility and indentation.
    Add `watch:dev` script for focused library development.

--- src/lib/util-component/mat-form-field-form/mat-form-field-form.component.ts
+++ src/lib/util-component/mat-form-field-form/mat-form-field-form.component.ts
@@ -1,5 +1,7 @@
-import { Component, EventEmitter, forwardRef, input } from '@angular/core';
+import { Component, EventEmitter, forwardRef, inject, Inject, input, signal } from '@angular/core';
 import { MatFormFieldComponent } from '../mat-form-field/mat-form-field.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { MatFormFieldInputDynamicFormComponent } from '../mat-form-field-input-dynamic-form/mat-form-field-input-dynamic-form.component';
 
 @Component({
   selector: 'app-mat-form-field-form',
@@ -10,6 +12,8 @@ import { MatFormFieldComponent } from '../mat-form-field/mat-form-field.componen
 })
 export class MatFormFieldFormComponent extends MatFormFieldComponent {
 
+  isDialog = signal(false);
+
   saveLabel = input<string>('Save');
   revertLabel = input<string>('Revert');
   removeLabel = input<string>('Delete');
@@ -17,6 +21,18 @@ export class MatFormFieldFormComponent extends MatFormFieldComponent {
   onDelete: EventEmitter<void> = new EventEmitter<void>();
   onRevert: EventEmitter<void> = new EventEmitter<void>();
   onSave: EventEmitter<void> = new EventEmitter<void>();
+
+  @Inject(MAT_DIALOG_DATA) dialogData?: any;
+  dialogRef?: MatDialogRef<MatFormFieldInputDynamicFormComponent> = inject(MatDialogRef<MatFormFieldInputDynamicFormComponent>);
+
+  override ngOnInit(): void {
+    super.ngOnInit();
+    this.value = structuredClone(this.value);
+
+    if(this.dialogData) {
+      this.isDialog.set(true);
+    }
+  }
 
   save(): void {
     this.valueChange.emit(this.value);
--- src/lib/util-component/mat-form-field-input-dynamic-form/mat-form-field-input-dynamic-form.component.ts
+++ src/lib/util-component/mat-form-field-input-dynamic-form/mat-form-field-input-dynamic-form.component.ts
@@ -1,10 +1,12 @@
-import { Component, computed, EventEmitter, forwardRef, Input, input, signal } from '@angular/core';
+import { Component, computed, EventEmitter, forwardRef, inject, Inject, Input, input, OnInit, signal } from '@angular/core';
 import { NgComponentModule } from "../../module/ng-component.module";
 import { MatFormFieldComponent } from '../mat-form-field/mat-form-field.component';
 import { ValueTracking } from '../../abtract/valueTracking.directive';
 import { ViesForm } from '../../model/vies.model';
 import { MatFormFieldFormComponent } from '../mat-form-field-form/mat-form-field-form.component';
 import { MatFormFieldInputDynamicComponent } from '../mat-form-field-input-dynamic/mat-form-field-input-dynamic.component';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+import { runInThisContext } from 'vm';
 
 @Component({
   selector: 'app-mat-form-field-input-dynamic-form',
@@ -15,6 +17,9 @@ import { MatFormFieldInputDynamicComponent } from '../mat-form-field-input-dynam
 })
 export class MatFormFieldInputDynamicFormComponent extends MatFormFieldFormComponent {
 
-  isDialog = input<boolean>(false);
   validInput = signal<boolean>(false);
+
+  hideRevertButton = input<boolean>(false);
+  hideRemoveButton = input<boolean>(false);
+  indent = input<boolean>(false);
 }
