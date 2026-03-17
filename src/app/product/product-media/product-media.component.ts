9906f185b9b308e30bdf6fec74b3ba0f2efb03d1
VincentB00 <vincentbui1999@gmail.com>
2026-03-17 16:36:27 -0400

    add product media

---
+++ src/app/product/product-media/product-media.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-media',
+  templateUrl: './product-media.component.html',
+  styleUrls: ['./product-media.component.scss'],
+  imports: []
+})
+export class ProductMediaComponent {
+  constructor() { }
+}
